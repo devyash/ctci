561800802b60b67cbf1691a050bf99223ad0e53c
Rushabh Patel <irushabhpatel@gmail.com>
2017-07-25 15:34:45 -0400

    Q8.2 of CTCI. Robot grid using recursion

---
+++ Recursion/recursion_8_2.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class recursion_8_2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter X:");
+        int x = scanner.nextInt();
+        System.out.println("enter Y:");
+        int y = scanner.nextInt();
+        System.out.println("possible paths are " + (fact(x + y - 2)) / (fact(x-1) * (fact(y-1)) )) ;
+    }
+    static int fact(int n){
+        if(n==1){
+            return 1;
+        }else{
+            return n * fact(n-1);
+        }
+    }
+}
